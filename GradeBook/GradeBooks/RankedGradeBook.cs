6d112b71207f3e8772c11b026825752d5f949aac
Daniel Mishne <danielmishne@microsoft.com>
2021-02-19 16:21:44 +0200

    step 4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -22,12 +22,32 @@ namespace GradeBook.GradeBooks
             }
 
             var ordererdStudents = Students.OrderByDescending(student => student.AverageGrade).ToList();
-            if (averageGrade >= ordererdStudents[gradeRangeSize- 1].AverageGrade) { return 'A'; }
+            if (averageGrade >= ordererdStudents[gradeRangeSize - 1].AverageGrade) { return 'A'; }
             if (averageGrade >= ordererdStudents[gradeRangeSize * 2 - 1].AverageGrade) { return 'B'; }
             if (averageGrade >= ordererdStudents[gradeRangeSize * 3 - 1].AverageGrade) { return 'C'; }
             if (averageGrade >= ordererdStudents[gradeRangeSize * 4 - 1].AverageGrade) { return 'D'; }
             return 'F';
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Where(student => student.Grades.Count > 0).Count() < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
